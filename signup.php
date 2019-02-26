98e480d6d7cf1e96430d0c157d82048ac339ff91
Fr3ud <oo.pavelko@gmail.com>
2019-02-26 16:39:43 +0200

    Add signup.php

---
+++ signup.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Camagru - Sign up</title>
+</head>
+<body>
+  <?php include('header.php') ?>
+  <div id="login">
+    <form method="post" action="forms/newuser.php">
+      <label for="username">Username: </label>
+      <input id="username" name="username" placeholder="Username" type="text">
+      <label for="mail">Email: </label>
+      <input id="mail" name="mail" placeholder="name@mail.com" type="mail">
+      <label for="password">Password: </label>
+      <input id="password" name="password" placeholder="Password" type="password">
+      <input type="submit" name="submit" value="Sign up">
+      <span>
+        <?php
+        if ($_SESSION['error']) {
+          echo $_SESSION['error'];
+        }
+        $_SESSION['error'] = null;
+        if (isset($_SESSION['signup'])) {
+          echo "Success! Check your email!";
+          $_SESSION['signup'] = null;
+        }
+        ?>
+      </span>
+    </form>
+  </div>
+  <?php include('footer.php') ?>
+</body>
+</html>
