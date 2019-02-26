01a941f7edb898731ce6c1d9b88ebfbd0bd46cc8
Fr3ud <oo.pavelko@gmail.com>
2019-02-26 18:31:44 +0200

    Add header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<header id="header">
+  <?php if (isset($_SESSION['id'])) { ?>
+    <div class="button" onclick="location.href='gallery.php'">
+      <span>Gallery</span>
+    </div>
+    <div class="button" onclick="location.href='views.php'">
+      <span>Views</span>
+    </div>
+    <div class="button" onclick="location.href='forms/logout.php'">
+      <span>Logout</span>
+    </div>
+  <?php } else { ?>
+    <div class="button" onclick="location.href='index.php'">
+      <span>Login</span>
+    </div>
+  <?php } ?>
+</header>
